0521c9799285932f61a2e600145c85e2cc6209f0
cesar cornejo <cesar.cornejo.l>
2015-07-13 03:14:28 -0500

    Despliegue Heroku

---
+++ models/models.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
+
+// Importar definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz;
+
+// sequelize.sync() crea e inicializa tabla de preguntas
+sequelize.sync().success(function() 
+{
+	Quiz.count().success(function (count)
+	{
+    		if(count === 0) 
+		{   // la tabla se inicializa solo si está
+			Quiz.create(
+			{ 
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+			.success(function()
+			{
+				console.log('Base de datos inicializada')});
+		};
+	});			
+});
+     
