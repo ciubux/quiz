08b850c0cacb6c6186ed6c0dac9f2f691febd096
cesar cornejo <cesar.cornejo.l>
2015-08-02 18:10:23 -0500

    modulo8

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []  });
 });
 
 router.param('quizId',quizController.load);
@@ -19,6 +19,11 @@ router.get('/author', function(req, res) {
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 //router.get('/quizes/question',quizController.question);
 //router.get('/quizes/answer',quizController.answer);
 
