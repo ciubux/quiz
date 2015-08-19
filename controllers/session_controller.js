eae83b7d8b9c2cead067cae7bbe1c8b900ae61f0
cesar cornejo <cesar.cornejo.l>
2015-08-19 00:39:35 -0500

    ejercicio 10

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,7 +1,36 @@
+var getfecha = function getDateTime(date) {
+	    var hour = date.getHours();
+	    hour = (hour < 10 ? "0" : "") + hour;
+	    var min  = date.getMinutes();
+	    min = (min < 10 ? "0" : "") + min;
+	    var sec  = date.getSeconds();
+	    sec = (sec < 10 ? "0" : "") + sec;
+	    var year = date.getFullYear();
+	    var month = date.getMonth() + 1;
+	    month = (month < 10 ? "0" : "") + month;
+	    var day  = date.getDate();
+	    day = (day < 10 ? "0" : "") + day;
+	    return year + "" + month + "" + day + "" + hour + "" + min + "" + sec;
+	};
+
 // MW de autorización de accesos HTTP restringidos
 exports.loginRequired = function(req, res, next){
-    if (req.session.user) {
-        next();
+    if (req.session.user) {	
+	
+	var fechaActual = new Date();
+        fechaActual.setMinutes(fechaActual.getMinutes()-2);
+	var fechaSession = req.session.fecha;
+	console.log('Fecha actual: '+getfecha(fechaActual));
+	console.log('Fecha session: '+fechaSession);
+
+	if(getfecha(fechaActual) > fechaSession )
+	{
+		 res.redirect('/login');	
+	}
+	else
+	{
+	        next();
+	}
     } else {
         res.redirect('/login');
     }
@@ -34,6 +63,7 @@ exports.create = function(req, res) {
         // Crear req.session.user y guardar campos   id  y  username
         // La sesión se define por la existencia de:    req.session.user
         req.session.user = {id:user.id, username:user.username};
+	req.session.fecha = getfecha(new Date());
 
         res.redirect(req.session.redir.toString());// redirección a path anterior a login
     });
